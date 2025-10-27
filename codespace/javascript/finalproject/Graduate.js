9e64cc393771ac064a329490c83460c4b84317ae
Morganryan1 <56196774+Morganryan1@users.noreply.github.com>
2025-10-26 22:38:36 -0400

    Update Graduate.js

--- codespace/javascript/finalproject/Graduate.js
+++ codespace/javascript/finalproject/Graduate.js
@@ -1,15 +1,15 @@
-{
+var hotelLocation = {
   "type": "FeatureCollection",
   "features": [
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [ -96.708968, 40.814475 ]
+        "coordinates": [ -96.708968, 40.814475 ] // [longitude, latitude]
       },
       "properties": {
         "Hotel": "The Graduate"
       }
     }
   ]
-}
+};
