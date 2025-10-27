5300271b6a6a7301759ea46fc805020fbdb4ce69
Morganryan1 <morganryan2014M@gmail.com>
2025-10-26 22:19:06 -0400

    Graduate school project add

---
+++ codespace/javascript/finalproject/Graduate.js
@@ -0,0 +1,15 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [ -96.708968, 40.814475 ]
+      },
+      "properties": {
+        "Hotel": "The Graduate"
+      }
+    }
+  ]
+}
---
+++ codespace/javascript/finalproject/LincolnFood.js
@@ -0,0 +1,485 @@
+var FoodData = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70230556,40.80666667 ]
+    },
+    "properties": {
+    "Restaurants":"Billy's",
+    "Price":"$$$",
+    "Vegan":"No",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"American",
+    "Website":"<a href=\"http://www.billysrestaurant.com\">Billys Website</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70530556,40.81447222 ]
+    },
+    "properties": {
+    "Restaurants":"DISH",
+    "Price":"$$$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"American",
+    "Website":"<a href=\"https://dishdowntown.com\">Dish Downtown Website</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.705296,40.814973 ]
+    },
+    "properties": {
+    "Restaurants":"Misty's",
+    "Price":"$$$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"Steakhouse",
+    "Website":"<a href=\"https://www.mistyslincoln.com/Home\">Mistys Downtown</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.710583,40.814551 ]
+    },
+    "properties": {
+    "Restaurants":"Rodizio Grill",
+    "Price":"$$$",
+    "Vegan":"No",
+    "Vegetarian":"No",
+    "GlutenFriendly":"No",
+    "Cuisine":"Brazillian",
+    "Website":"<a href=\"https://www.rodiziogrill.com/\">Rodizio Grill</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.71019444,40.81505556 ]
+    },
+    "properties": {
+    "Restaurants":"The Oven",
+    "Price":"$$",
+    "Vegan":"Yes",
+    "Vegetarian":"No",
+    "GlutenFriendly":"No",
+    "Cuisine":"Indian",
+    "Website":"<a href=\"https://theoven-lincoln.com/locations-and-menus/haymarket\">The Oven Haymarket Location</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70658333,40.81447222 ]
+    },
+    "properties": {
+    "Restaurants":"Single Barrel",
+    "Price":"$$",
+    "Vegan":"No",
+    "Vegetarian":"No",
+    "GlutenFriendly":"No",
+    "Cuisine":"Steakhouse",
+    "Website":"<a href=\"http://thesinglebarrel.com/\">The Single Barrel</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.71080556,40.81502778 ]
+    },
+    "properties": {
+    "Restaurants":"Lazlo's",
+    "Price":"$$",
+    "Vegan":"No",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"Brewpub",
+    "Website":"<a href=\"http://www.lazlosbreweryandgrill.com/\">Lazlos Downtown</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.71213889,40.8165 ]
+    },
+    "properties": {
+    "Restaurants":"Hiro 88",
+    "Price":"$$",
+    "Vegan":"No",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"Pan-Asian",
+    "Website":"<a href=\"https://hiro88.com/\">Hiro 88 Downtown</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.71022222,40.81608333 ]
+    },
+    "properties": {
+    "Restaurants":"Leadbelly",
+    "Price":"$$",
+    "Vegan":"No",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"American",
+    "Website":"<a href=\"https://www.getleaded.com/\">Leadbelly Website</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70666667,40.81083333 ]
+    },
+    "properties": {
+    "Restaurants":"Green Gateau",
+    "Price":"$$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"French, Bistro",
+    "Website":"<a href=\"https://www.greengateau.com/\">The Green Gateau</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.71016667,40.81555556 ]
+    },
+    "properties": {
+    "Restaurants":"Buzzard Billy's",
+    "Price":"$$",
+    "Vegan":"No",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"American, Creole, Cajun",
+    "Website":"<a href=\"https://www.buzzardbillys.com/lincoln/\">Buzzard Billys Lincoln</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.7095,40.81741667 ]
+    },
+    "properties": {
+    "Restaurants":"Blue Sushi Sake Grill",
+    "Price":"$$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"Sushi",
+    "Website":"<a href=\"https://bluesushisakegrill.com/locations/nebraska/lincoln/haymarket\">Blue Sushi in the Haymarket</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.71219444,40.81686111 ]
+    },
+    "properties": {
+    "Restaurants":"Mellow Mushroom",
+    "Price":"$$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"Pizza",
+    "Website":"<a href=\"https://mellowmushroom.com/location/ne-lincoln-601-r-st-ste-110-68508/\">Mellow Mushroom Lincoln</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.7095,40.81497222 ]
+    },
+    "properties": {
+    "Restaurants":"Vincenzo's",
+    "Price":"$$",
+    "Vegan":"No",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"Italian",
+    "Website":"<a href=\"https://vincenzos-ne.com/\">Vincenzos</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70772222,40.81416667 ]
+    },
+    "properties": {
+    "Restaurants":"Blue Orchid Thai",
+    "Price":"$$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"Thai",
+    "Website":"<a href=\"https://www.restaurantji.com/ne/lincoln/blue-orchid-thai-restaurant-/\">The Blue Orchid</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70736111,40.81430556 ]
+    },
+    "properties": {
+    "Restaurants":"Grey Whale ",
+    "Price":"$$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"Sushi, Pan-Asian",
+    "Website":"<a href=\"https://www.greywhalesushi.com/\">Gray Whale Sushi</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70972222,40.81563889 ]
+    },
+    "properties": {
+    "Restaurants":"Screamers Dining and Cabaret",
+    "Price":"$$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"American",
+    "Website":"<a href=\"https://www.screamersdining.com/\">Screamers</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.71155556,40.81613889 ]
+    },
+    "properties": {
+    "Restaurants":"BurgerFi",
+    "Price":"$$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"American",
+    "Website":"<a href=\"https://order.burgerfi.com/locations/NE/lincoln/20463\"><BurgerFi Lincoln</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.71133333,40.81616667 ]
+    },
+    "properties": {
+    "Restaurants":"Gate 25",
+    "Price":"$$",
+    "Vegan":"No",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"American",
+    "Website":"<a href=\"https://gate25lnk.com/\">Gate 25</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.700191,40.813983 ]
+    },
+    "properties": {
+    "Restaurants":"Rutabagas",
+    "Price":"$$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"Plant-based, American",
+    "Website":"<a href=\"https://www.eatrutabagas.com/\">Rutabagas</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70222222,40.81327778 ]
+    },
+    "properties": {
+    "Restaurants":"Sultan's Kite",
+    "Price":"$",
+    "Vegan":"No",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"Mediterranean, Halal",
+    "Website":"<a href=\"https://sultanskite.com/\">Sultans Kite</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70097222,40.81394444 ]
+    },
+    "properties": {
+    "Restaurants":"Ali Baba Gyros",
+    "Price":"$",
+    "Vegan":"No",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"Greek, Mediterranean, Halal",
+    "Website":"<a href=\"https://www.facebook.com/AliBabaGyros\">Alibab Gyros Facebook</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.69997222,40.81344444 ]
+    },
+    "properties": {
+    "Restaurants":"AmuManu",
+    "Price":"$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"Asian",
+    "Website":"<a href=\"https://www.amumanuramenbar.com/\">Amu Manu Ramen</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70291667,40.81544444 ]
+    },
+    "properties": {
+    "Restaurants":"Thai Garden",
+    "Price":"$",
+    "Vegan":"No",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"No",
+    "Cuisine":"Thai",
+    "Website":"<a href=\"https://downtownlincoln.org/go/thai-garden\">Thai Garden</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70894444,40.81563889 ]
+    },
+    "properties": {
+    "Restaurants":"Barry's",
+    "Price":"$",
+    "Vegan":"No",
+    "Vegetarian":"No",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"American",
+    "Website":"<a href=\"https://barrysbarandgrill.com/\">Barrys Bar and Grill</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70452778,40.81497222 ]
+    },
+    "properties": {
+    "Restaurants":"Qdoba",
+    "Price":"$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"Mexican",
+    "Website":"<a href\"https://locations.qdoba.com/us/ne/lincoln/211-n-12th-st.html\">Qdoba Downtown</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70238889,40.81575 ]
+    },
+    "properties": {
+    "Restaurants":"Chipotle",
+    "Price":"$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"Mexican",
+    "Website":"<a href=\"https://locations.chipotle.com/ne/lincoln/1317-q-st\">Chipotle Downtown</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70180556,40.81566667 ]
+    },
+    "properties": {
+    "Restaurants":"Panda Express",
+    "Price":"$",
+    "Vegan":"No",
+    "Vegetarian":"No",
+    "GlutenFriendly":"No",
+    "Cuisine":"Chinese",
+    "Website":"<a href=\"https://www.pandaexpress.com/userlocation/2190/ne/lincoln/1317-q-street-suite-140\">Panda Express Downtown</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70141667,40.81491667 ]
+    },
+    "properties": {
+    "Restaurants":"Raising Cane's",
+    "Price":"$",
+    "Vegan":"No",
+    "Vegetarian":"No",
+    "GlutenFriendly":"No",
+    "Cuisine":"American",
+    "Website":"<a href=\"https://raisingcanes.com/location/nebraska/lincoln/love-shack\">Raising Canes Downtown</a>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -96.70236111,40.81541667 ]
+    },
+    "properties": {
+    "Restaurants":"Blaze Pizza",
+    "Price":"$",
+    "Vegan":"Yes",
+    "Vegetarian":"Yes",
+    "GlutenFriendly":"Yes",
+    "Cuisine":"Pizza",
+    "Website":"<a href=\"https://hq.blazepizza.com/locations/lincoln/\">Blaze Pizza Lincoln</a>"
+    }
+  }
+]
+};
